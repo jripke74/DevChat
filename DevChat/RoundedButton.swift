588a55d6e7731709d4dee6d2a510395b221e6d15
Jeff Ripke <jripke74@gmail.com>
2017-06-28 13:19:36 -0700

    add login fields and graphics

---
+++ DevChat/RoundedButton.swift
@@ -0,0 +1,37 @@
+//
+//  RoundedButton.swift
+//  DevChat
+//
+//  Created by Jeff Ripke on 6/28/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var bgColor: UIColor? {
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+}
